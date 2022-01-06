6888a5ce31c93c592ee228f179e5974e3dd5c524
KarlHendrikBachmann <71638019+KarlHendrikBachmann@users.noreply.github.com>
2022-01-06 22:59:47 +0200

    Changing mqtt library

--- src/main.cpp
+++ src/main.cpp
@@ -1,64 +1,161 @@
+#include <Arduino.h>
 #include "bsec.h"
-#include <WiFi.h>
-#include <PubSubClient.h>
 #include <Wire.h>
-#include "credentials.h"
+#include <WiFi.h>
+extern "C" {
+	#include "freertos/FreeRTOS.h"
+	#include "freertos/timers.h"
+}
+#include <AsyncMqttClient.h>
+#include <credentials.h>
+
 
 //Wifi info and debugging led
 int LED_BUILTIN = 2;
 
-const char* ssid     = WIFINAME;
-const char* password = WIFIPASS;
-const char* url = URL; //Endpoint server
-const char* tenant = TENANT;
-const char* passwordiot = IOTPASS;
 String clientId = "KarliESP-PimoroniBme680-1";
 String command = "";
+String TOPIC = "s/us";
+unsigned long lastMsg = 0;
 
+AsyncMqttClient mqttClient;
+TimerHandle_t mqttReconnectTimer;
+TimerHandle_t wifiReconnectTimer;
 
-WiFiClient wifiClient;
-PubSubClient mqttClient(wifiClient);
-
-// Helper functions declarations
-void checkIaqSensorStatus(void);
-void reconnect();
-void alarmSend(String,bool);
-
-// Create an object of the class Bsec
 Bsec iaqSensor;
 
 String output;
 int sda = 21;
 int scl = 23;
 
-// Entry point for the example
-void setup(void)
+void connectToWifi() {
+  Serial.println("Connecting to Wi-Fi...");
+  WiFi.begin(WIFINAME, WIFIPASS);
+}
+
+void connectToMqtt() {
+  Serial.println("Connecting to MQTT...");
+  mqttClient.connect();
+  command = "100,"+clientId+",c8y_MQTTdevice";
+  mqttClient.publish(TOPIC.c_str(), 0, false, command.c_str());
+}
+
+void WiFiEvent(WiFiEvent_t event) {
+    Serial.printf("[WiFi-event] event: %d\n", event);
+    switch(event) {
+    case SYSTEM_EVENT_STA_GOT_IP:
+        Serial.println("WiFi connected");
+        Serial.println("IP address: ");
+        Serial.println(WiFi.localIP());
+        connectToMqtt();
+        break;
+    case SYSTEM_EVENT_STA_DISCONNECTED:
+        Serial.println("WiFi lost connection");
+        xTimerStop(mqttReconnectTimer, 0); // ensure we don't reconnect to MQTT while reconnecting to Wi-Fi
+        xTimerStart(wifiReconnectTimer, 0);
+        break;
+    default:
+      Serial.println("Unhandled wifi event just happened!");
+    }
+}
+
+void onMqttConnect(bool sessionPresent) {
+  digitalWrite(LED_BUILTIN, LOW);
+  Serial.println("Connected to MQTT.");
+  Serial.print("Session present: ");
+  Serial.println(sessionPresent);
+}
+
+void onMqttDisconnect(AsyncMqttClientDisconnectReason reason) {
+  digitalWrite(LED_BUILTIN, HIGH);
+  Serial.println("Disconnected from MQTT.");
+
+  if (WiFi.isConnected()) {
+    xTimerStart(mqttReconnectTimer, 0);
+  }
+}
+
+void onMqttSubscribe(uint16_t packetId, uint8_t qos) {
+  Serial.println("Subscribe acknowledged.");
+  Serial.print("  packetId: ");
+  Serial.println(packetId);
+  Serial.print("  qos: ");
+  Serial.println(qos);
+}
+
+void onMqttUnsubscribe(uint16_t packetId) {
+  Serial.println("Unsubscribe acknowledged.");
+  Serial.print("  packetId: ");
+  Serial.println(packetId);
+}
+
+void onMqttMessage(char* topic, char* payload, AsyncMqttClientMessageProperties properties, size_t len, size_t index, size_t total) {
+  Serial.println("Publish received.");
+  Serial.print("  topic: ");
+  Serial.println(topic);
+  Serial.print("  qos: ");
+  Serial.println(properties.qos);
+  Serial.print("  dup: ");
+  Serial.println(properties.dup);
+  Serial.print("  retain: ");
+  Serial.println(properties.retain);
+  Serial.print("  len: ");
+  Serial.println(len);
+  Serial.print("  index: ");
+  Serial.println(index);
+  Serial.print("  total: ");
+  Serial.println(total);
+}
+
+void onMqttPublish(uint16_t packetId) {
+  Serial.println("Publish acknowledged.");
+  Serial.print("  packetId: ");
+  Serial.println(packetId);
+}
+
+// Helper function definitions
+void checkIaqSensorStatus(void)
 {
+  if (iaqSensor.status != BSEC_OK) {
+    if (iaqSensor.status < BSEC_OK) {
+      output = "BSEC error code : " + String(iaqSensor.status);
+      Serial.println(output);
+    } else {
+      output = "BSEC warning code : " + String(iaqSensor.status);
+      Serial.println(output);
+    }
+  }
+
+  if (iaqSensor.bme680Status != BME680_OK) {
+    if (iaqSensor.bme680Status < BME680_OK) {
+      output = "BME680 error code : " + String(iaqSensor.bme680Status);
+      Serial.println(output);
+    } else {
+      output = "BME680 warning code : " + String(iaqSensor.bme680Status);
+      Serial.println(output);
+    }
+  }
+
+  while(true) {
+    digitalWrite(LED_BUILTIN, HIGH);
+    delay(2500);
+    digitalWrite(LED_BUILTIN, LOW);
+    delay(2500);
+  }
+}
+
+void setup() {
+  //Start serial and LED
   Serial.begin(115200);
   while(!Serial);
-  //=========================================
-  // We start by connecting to a WiFi network
-  Serial.println();
-  Serial.print("Connecting to ");
-  Serial.println(ssid);
-
-  WiFi.begin(ssid, password);
   pinMode(LED_BUILTIN, OUTPUT);
   digitalWrite(LED_BUILTIN, HIGH);
-  while (WiFi.status() != WL_CONNECTED) {
-      delay(500);
-      Serial.print(".");
-  }
-  Serial.println("WiFi connected");
-  //=========================================
-  //MQTT
-  mqttClient.setServer(url, 1883);
-  reconnect();
-  //=========================================
-  digitalWrite(LED_BUILTIN, LOW);
 
-  //=========================================
-  Wire.begin(scl,sda);
+  Serial.println();
+  Serial.println();
+
+  //Sensor setup
+  Wire.begin(sda,scl);
 
   iaqSensor.begin(BME680_I2C_ADDR_PRIMARY, Wire);
   output = "\nBSEC library version " + String(iaqSensor.version.major) + "." + String(iaqSensor.version.minor) + "." + String(iaqSensor.version.major_bugfix) + "." + String(iaqSensor.version.minor_bugfix);
@@ -81,121 +178,55 @@ void setup(void)
   iaqSensor.updateSubscription(sensorList, 10, BSEC_SAMPLE_RATE_LP);
   checkIaqSensorStatus();
 
-  // Print the header
-  output = "Timestamp [ms], raw temperature [°C], pressure [hPa], raw relative humidity [%], gas [Ohm], IAQ, IAQ accuracy, temperature [°C], relative humidity [%], Static IAQ, CO2 equivalent, breath VOC equivalent";
-  Serial.println(output);
+  //Create MQTT timers
+  mqttReconnectTimer = xTimerCreate("mqttTimer", pdMS_TO_TICKS(2000), pdFALSE, (void*)0, reinterpret_cast<TimerCallbackFunction_t>(connectToMqtt));
+  wifiReconnectTimer = xTimerCreate("wifiTimer", pdMS_TO_TICKS(2000), pdFALSE, (void*)0, reinterpret_cast<TimerCallbackFunction_t>(connectToWifi));
+
+  //Create WIFI state listener
+  WiFi.onEvent(WiFiEvent);
+
+  //Create MQTT state methods
+  mqttClient.onConnect(onMqttConnect);
+  mqttClient.onDisconnect(onMqttDisconnect);
+  mqttClient.onSubscribe(onMqttSubscribe);
+  mqttClient.onUnsubscribe(onMqttUnsubscribe);
+  mqttClient.onMessage(onMqttMessage);
+  mqttClient.onPublish(onMqttPublish);
+  mqttClient.setServer(URL, 1883);
+  mqttClient.setCredentials(TENANT,IOTPASS);
+  mqttClient.setClientId(clientId.c_str());
+
+  //Start connection
+  connectToWifi();
 }
 
-void reconnect() {
-  // Loop until we're reconnected
-  while (!mqttClient.connected()) {
-    digitalWrite(LED_BUILTIN, HIGH);
-    Serial.print("Attempting MQTT connection...");
-    // Attempt to connect
-    if (mqttClient.connect(clientId.c_str(),tenant,passwordiot)) {
-      Serial.println("Connected");
-      String command = "100,"+clientId+",c8y_MQTTdevice";
-      mqttClient.publish("s/us", (char*) command.c_str());
-      digitalWrite(LED_BUILTIN, LOW);
-      // Connected - do something useful - subscribe to topics, publish messages, etc.
-    } else {
-      Serial.print("failed, rc=");
-      Serial.print(mqttClient.state());
-      // Wait 5 seconds before retrying
-      Serial.println("Disconnecting wifi");
-      WiFi.disconnect();
-      Serial.println("Reconnecting wifi in 5 seconds");
-      Serial.print("Connecting to ");
-      Serial.println(ssid);
-      delay(5000);
-      WiFi.reconnect();
-      int wifiCounter = 0;
-      while (WiFi.status() != WL_CONNECTED) {
-        delay(500);
-        Serial.print(".");
-        wifiCounter +=1;
-        if(wifiCounter > 30) {
-          Serial.println("Wifi reconnect timed out. Restarting ESP.");
-          ESP.restart();
-        }
-      }
-    }
-  }
-}
-
-// Function that is looped forever
-void loop(void) {
-  if (!mqttClient.connected()) {
-    reconnect();
-  }
-
-  mqttClient.loop();
-  
-  unsigned long time_trigger = millis();
-  if (iaqSensor.run()) { // If new data is available
-    output = String(time_trigger);
-    output += ", " + String(iaqSensor.pressure);
-    output += ", " + String(iaqSensor.iaq);
-    output += ", " + String(iaqSensor.temperature);
-    output += ", " + String(iaqSensor.humidity);
-    output += ", " + String(iaqSensor.co2Equivalent);
-    output += ", " + String(iaqSensor.breathVocEquivalent);
-    Serial.println(output);
-    command = "200,pressureMeasurement,hecto pascals,"+String(iaqSensor.pressure)+",hPa";
-    mqttClient.publish("s/us", (char*) command.c_str());
-    command = "200,iaqMeasurement,indoor air quality,"+String(iaqSensor.iaq)+",iaq";
-    mqttClient.publish("s/us", (char*) command.c_str());
-    command = "211,"+String(iaqSensor.temperature);
-    mqttClient.publish("s/us", (char*) command.c_str());
-    command = "200,humidityMeasurement,percent,"+String(iaqSensor.humidity)+",%";
-    mqttClient.publish("s/us", (char*) command.c_str());
-    command = "200,eco2Measurement,particles per million,"+String(iaqSensor.co2Equivalent)+",ppm";
-    mqttClient.publish("s/us", (char*) command.c_str());
-    command = "200,ebvoc,particles per million,"+String(iaqSensor.breathVocEquivalent)+",ppm";
-    mqttClient.publish("s/us", (char*) command.c_str());
-  } else {
-    checkIaqSensorStatus();
-  }
-}
-
-// Helper function definitions
-void checkIaqSensorStatus(void)
-{
-  if (iaqSensor.status != BSEC_OK) {
-    if (iaqSensor.status < BSEC_OK) {
-      output = "BSEC error code : " + String(iaqSensor.status);
+void loop() {
+  unsigned long now = millis();
+  if (now - lastMsg > 5000) { // Wait 5 seconds between every request
+    if (iaqSensor.run()) { // If new data is available
+      lastMsg = now;
+      output = String(iaqSensor.pressure);
+      output += ", " + String(iaqSensor.iaq);
+      output += ", " + String(iaqSensor.temperature);
+      output += ", " + String(iaqSensor.humidity);
+      output += ", " + String(iaqSensor.co2Equivalent);
+      output += ", " + String(iaqSensor.breathVocEquivalent);
       Serial.println(output);
-    } else {
-      output = "BSEC warning code : " + String(iaqSensor.status);
-      Serial.println(output);
+      command = "200,pressureMeasurement,hecto pascals,"+String(iaqSensor.pressure)+",hPa";
+      mqttClient.publish(TOPIC.c_str(), 0, false, command.c_str());
+      command = "200,iaqMeasurement,indoor air quality,"+String(iaqSensor.iaq)+",iaq";
+      mqttClient.publish(TOPIC.c_str(), 0, false, command.c_str());
+      command = "211,"+String(iaqSensor.temperature);
+      mqttClient.publish(TOPIC.c_str(), 0, false, command.c_str());
+      command = "200,humidityMeasurement,percent,"+String(iaqSensor.humidity)+",%";
+      mqttClient.publish(TOPIC.c_str(), 0, false, command.c_str());
+      command = "200,eco2Measurement,particles per million,"+String(iaqSensor.co2Equivalent)+",ppm";
+      mqttClient.publish(TOPIC.c_str(), 0, false, command.c_str());
+      command = "200,ebvoc,particles per million,"+String(iaqSensor.breathVocEquivalent)+",ppm";
+      mqttClient.publish(TOPIC.c_str(), 0, false, command.c_str());
     }
-  }
-
-  if (iaqSensor.bme680Status != BME680_OK) {
-    if (iaqSensor.bme680Status < BME680_OK) {
-      output = "BME680 error code : " + String(iaqSensor.bme680Status);
-      alarmSend(output,true);
-    } else {
-      output = "BME680 warning code : " + String(iaqSensor.bme680Status);
-      alarmSend(output,false);
+    else {
+      checkIaqSensorStatus();
     }
-  }
-}
-
-void alarmSend(String message, bool finish) {
-  Serial.println(message);
-  if(finish) {
-    command = "301,c8y_CriticalAlarm,"+message;
-    mqttClient.publish("s/us", (char*) command.c_str());
-  }
-  else {
-    command = "304,c8y_WarningAlarm,"+message;
-    mqttClient.publish("s/us", (char*) command.c_str());
-  }
-  while(finish) {
-    digitalWrite(LED_BUILTIN, HIGH);
-    delay(2500);
-    digitalWrite(LED_BUILTIN, LOW);
-    delay(2500);
   }
 }
